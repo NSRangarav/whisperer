e1618b23bbd4d3b9eae76d23de15251f3e5e112c
Rangaramanujam N S <rangaramanujamsri@gmail.com>
2024-05-08 15:58:39 +0000

    commit# Please enter the commit message for your changes. Lines starting

--- whisperer-ai/tailwind.config.js
+++ whisperer-ai/tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-];
-export const theme = {
-  extend: {
-    fontFamily: {
-      sans: ['var(--font-inter)'],
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
     },
   },
-};
-export const plugins = [];
+  plugins: [],
+}
